789fff0be20943b34f9bcca12027dcaad07606d8
Glenn Neidermeier <neiderm@github.com>
2024-11-28 18:00:16 -0500

    galaga_basys3.vhd

---
+++ vhdl_galaga_rev_0_3_2018_05_06/basys3/galaga_basys3.srcs/sources_1/new/galaga_basys3.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/28/2024 04:35:03 PM
+-- Design Name: 
+-- Module Name: galaga_basys3 - struct
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity galaga_basys3 is
+    Port ( clk : in STD_LOGIC;
+           sw : in STD_LOGIC_VECTOR (15 downto 0));
+end galaga_basys3;
+
+architecture struct of galaga_basys3 is
+
+begin
+
+
+end struct;
